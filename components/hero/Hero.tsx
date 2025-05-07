4df4b9695f7a2acb4fcb4e4c2fd0535f71d1ea56
sabir222 <mrsabir4@gmail.com>
2025-05-07 04:27:28 +0100

    fixing: typo

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
 		<div className="justify-between  min-h-40 flex flex-row" >
 			<div className="w-2/3">
 				<p className="font-bold  text-xl always-gradient dark:always-gradient-two">Sabir Koutabi</p>
-				<p className="font-thin  text-md mb-4 ">full stack developer</p>
+				<p className="font-thin  text-md mb-4 ">Full stack developer</p>
 				<p className="font-thin text-lg"> I like building digital things that are simple, honest, and actually useful.</p>
 			</div>
 		</div>
